be595bd0cad79cfee0e5d48fed248192e80c08c6
mizzi-sesh <dulcinea.mizzi@gmail.com>
2025-04-21 23:04:11 +0100

    added custom upload button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ async function Images(){
 	const images = await getMyImages();
 	return (
 		<div className="flex flex-wrap justify-center gap-4 p-4">
-				{[...images, ...images, ...images, ...images ].map((image) => (
+				{images.map((image) => (
 					<div key={image.id} className="h-48 w-48 flex flex-col"> 
 						<Link href={`/img/${image.id}`}>
 							<Image 
