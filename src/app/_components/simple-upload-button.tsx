c5d4f54488c402552fe58e5bf0883872c79f5716
mizzi-sesh <dulcinea.mizzi@gmail.com>
2025-04-23 01:12:35 +0100

    Add loading spinner

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -38,15 +38,32 @@ export function UploadSVG() {
   )
 }
 
+function LoadingSpinnerSVG() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z" className="spinner_aj0A"/>
+    </svg>
+  ) 
+}
+
+function makeUploadToast() {
+  return 
+}
 
 export function SimpleUploadButton()  {
   const router = useRouter();
+
   const { inputProps } = useUploadThingInputProps("imageUploader", { 
-    onUploadBegin(){
-      toast("Uploading", {
-        duration: 100000,
-        id: "upload-begin",
-      });
+    onUploadBegin() {
+      toast(
+        <div className="flex gap-2 items-center">
+          <LoadingSpinnerSVG/><span className="text-lg">Uploading... </span>
+        </div>, 
+        {
+          duration: 100000,
+          id: "upload-begin",
+        },
+      )
     },
     onClientUploadComplete(){
       toast.dismiss("upload-begin");
